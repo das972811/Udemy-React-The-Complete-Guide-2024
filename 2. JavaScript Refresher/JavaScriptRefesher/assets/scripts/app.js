68a271803ea4a8705d2cbbac3f0d04de6cbe1cfc
das972811 <salasdiego54@gmail.com>
2024-03-06 23:27:34 -0600

    Downloaded project and learned js modules

---
+++ 2. JavaScript Refresher/JavaScriptRefesher/assets/scripts/app.js
@@ -0,0 +1,14 @@
+// import apiKeyDefault, { apiKey } from './uitl.js';
+
+// import * as util from './uitl.js';
+
+import apiKeyDefault, { anotherApi as diego } from './uitl.js';
+
+// console.log(apiKey);
+// console.log(apiKeyDefault);
+
+// console.log(util.default);
+// console.log(util.anotherApi);
+
+console.log(apiKeyDefault);
+console.log(diego);
---
+++ 2. JavaScript Refresher/JavaScriptRefesher/assets/scripts/uitl.js
@@ -0,0 +1,4 @@
+export let apiKey = 'diego was here'
+export let anotherApi = 'anotherApi';
+
+export default 'export default';
