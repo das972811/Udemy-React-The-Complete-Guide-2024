4f154c1a62d3f6ac131e52ac19de16bd507273b3
das972811 <salasdiego54@gmail.com>
2024-03-07 22:15:18 -0600

    Basic js refesher

--- 2. JavaScript Refresher/JavaScriptRefesher/assets/scripts/app.js
+++ 2. JavaScript Refresher/JavaScriptRefesher/assets/scripts/app.js
@@ -1,14 +1,66 @@
-// import apiKeyDefault, { apiKey } from './uitl.js';
+// // import apiKeyDefault, { apiKey } from './uitl.js';
 
-// import * as util from './uitl.js';
+// // import * as util from './uitl.js';
 
-import apiKeyDefault, { anotherApi as diego } from './uitl.js';
+// import apiKeyDefault, { anotherApi as diego } from './uitl.js';
 
-// console.log(apiKey);
+// // console.log(apiKey);
+// // console.log(apiKeyDefault);
+
+// // console.log(util.default);
+// // console.log(util.anotherApi);
+
 // console.log(apiKeyDefault);
+// console.log(diego);
 
-// console.log(util.default);
-// console.log(util.anotherApi);
+let userMessage = 'diego is awesome at programming';
+console.log(userMessage);
 
-console.log(apiKeyDefault);
-console.log(diego);
+export default userName => 
+{
+    return 'diego was here';
+}
+
+const test = {
+    name: 'Max',
+    age: 34,
+    greet() {
+        console.log(this.name);
+    }
+}
+
+class User {
+    constructor() {
+
+    }
+
+    greet() {
+        console.log('diego was here');
+    }
+}
+
+let d = new User();
+
+d.greet();
+
+
+const hobbies = ['Swimming', 'Running', 'Losing'];
+
+hobbies.findIndex(item => item === "Swimming");
+
+const editedHobbies = hobbies.map(item => ({ text: item }))
+
+hobbies.map(item => item + "!");
+
+// Array Destructuring
+const [firstName, lastName] = ['Diego', 'Salas'];
+
+// Object Destructuring
+
+const { name, age } = { name: 'diego', age: 23 };
+
+
+// Spread Operators
+
+const newHobbies = ["Losing"];
+const test123 = [...newHobbies, ...hobbies];
