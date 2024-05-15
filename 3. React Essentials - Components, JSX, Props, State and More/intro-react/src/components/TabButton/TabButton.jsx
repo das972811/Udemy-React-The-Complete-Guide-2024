113d2ca1c66f324b43a74bca54d7aa82663b5ea9
Diego Polar <Diego.Polar@baldwincountyal.gov>
2024-05-14 19:05:42 -0500

    Cleaned code on Core Concepts using array.map

--- 3. React Essentials - Components, JSX, Props, State and More/intro-react/src/components/TabButton/TabButton.jsx
+++ 3. React Essentials - Components, JSX, Props, State and More/intro-react/src/components/TabButton/TabButton.jsx
@@ -5,11 +5,12 @@ TabButton.propTypes = {
         PropTypes.element,
         PropTypes.string
     ]).isRequired,
-    onSelect: PropTypes.func.isRequired
+    onSelect: PropTypes.func.isRequired,
+    isSelected: PropTypes.bool.isRequired
 };
 
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
     return (
-        <li><button className="" onClick={onSelect}>{children}</button></li>
+        <li><button className={isSelected ? 'active': undefined} onClick={onSelect}>{children}</button></li>
     );
 }
