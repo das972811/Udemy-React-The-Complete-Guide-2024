a031783ad4bbd7f5ad51de3a875ff642f69cfa5b
Diego Polar <Diego.Polar@baldwincountyal.gov>
2024-05-15 20:01:01 -0500

    Working on player component

--- 4. React Essentials - Deep Dive/tic-tac-toe/src/App.jsx
+++ 4. React Essentials - Deep Dive/tic-tac-toe/src/App.jsx
@@ -1,6 +1,15 @@
+import Player from "./components/Player/Player"
+
 function App() {
     return (
-        <h1>React Tic-Tac-Toe</h1>
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <Player name="Diego" symbol="X" />
+                    <Player name="Ignacio" symbol="O" />
+                </ol>
+            </div>
+        </main>
     )
 }
 
