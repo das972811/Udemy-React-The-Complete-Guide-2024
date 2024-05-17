8763636c07836a174f673b8332d020c40cf01a81
Diego Polar <Diego.Polar@baldwincountyal.gov>
2024-05-16 20:21:46 -0500

    Bug fixed on immutability

--- 4. React Essentials - Deep Dive/tic-tac-toe/src/components/GameOver/GameOver.jsx
+++ 4. React Essentials - Deep Dive/tic-tac-toe/src/components/GameOver/GameOver.jsx
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types';
 
 GameOver.propTypes = {
-    winner: PropTypes.string
+    winner: PropTypes.string,
+    onRestart: PropTypes.func
 };
 
-export default function GameOver({ winner })
+export default function GameOver({ winner, onRestart })
 {
     return (
         <div id="game-over">
             <h2>Game Over</h2>
             {winner && <p>{winner} won</p>}
             {!winner && <p>It&apos;s a draw!</p>}
-            <p><button>Rematch!</button></p>
+            <p><button onClick={onRestart}>Rematch!</button></p>
         </div>
     );
 }
