939249c4b3f24b6ad415c8a4b61dc0840371a77c
das972811 <salasdiego54@gmail.com>
2024-10-12 21:00:51 -0500

    Added geolocation base on user location

--- 15. Sending Http Request and Connecting to the Database/01-starting-project/src/components/AvailablePlaces.jsx
+++ 15. Sending Http Request and Connecting to the Database/01-starting-project/src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 import Places from './Places.jsx';
 import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
@@ -19,12 +20,16 @@ export default function AvailablePlaces({ onSelectPlace }) {
           const error = new Error('Failed to fetch places');
           throw error;
         }
-  
-        setAvailablePlaces(resData.places);
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(resData.places, position.coords.latitude, position.coords.longitude);
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch(error) {
         setError({message: error.message || 'Could not fetch places, please try again later.'});
+        setIsFetching(false);
       }
-      setIsFetching(false);
     }
     
     fetchPlaces();
