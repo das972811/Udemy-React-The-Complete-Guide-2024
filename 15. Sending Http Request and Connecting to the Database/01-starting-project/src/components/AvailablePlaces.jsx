ab7288d42e14f8650172def83dfef9537a687cab
das972811 <salasdiego54@gmail.com>
2024-10-12 20:25:51 -0500

    Getting places by http get and added loading state when fetching data

--- 15. Sending Http Request and Connecting to the Database/01-starting-project/src/components/AvailablePlaces.jsx
+++ 15. Sending Http Request and Connecting to the Database/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,10 +1,39 @@
+import { useState, useEffect } from 'react';
+
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
+
+  useEffect(() => {
+    setIsFetching(true);
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+
+    // fetch('http://localhost:3000/places').then((response) => {
+    //   return response.json();
+    // }).then((responseData) => {
+    //   setAvailablePlaces(responseData.places);
+    // });
+
+
+
+  }, []);
+
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
