e2df1fc9d4d0d249d32895808f6b7761cb262d6b
das972811 <salasdiego54@gmail.com>
2024-10-12 21:32:25 -0500

    Added updated request

--- 15. Sending Http Request and Connecting to the Database/01-starting-project/src/components/AvailablePlaces.jsx
+++ 15. Sending Http Request and Connecting to the Database/01-starting-project/src/components/AvailablePlaces.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
@@ -13,16 +14,10 @@ export default function AvailablePlaces({ onSelectPlace }) {
     setIsFetching(true);
     async function fetchPlaces() {
       try {
-        const response = await fetch('http://localhost:3000/places');
-        const resData = await response.json();
-  
-        if(!response.ok) {
-          const error = new Error('Failed to fetch places');
-          throw error;
-        }
+        const places = await fetchAvailablePlaces();
 
         navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(resData.places, position.coords.latitude, position.coords.longitude);
+          const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude);
           setAvailablePlaces(sortedPlaces);
           setIsFetching(false);
         });
