e730867ea5ca08943ecec851b85bfc59aa49438f
das972811 <salasdiego54@gmail.com>
2024-10-18 12:34:30 -0500

    Incorporated built-in browser validation

--- 17. Working with Forms and User Inputs/01-starting-project/src/App.jsx
+++ 17. Working with Forms and User Inputs/01-starting-project/src/App.jsx
@@ -8,8 +8,8 @@ function App() {
     <>
       <Header />
       <main>
-        {/* <Signup /> */}
-        <LoginRef />
+        <Signup />
+        {/* <LoginRef /> */}
       </main>
     </>
   );
