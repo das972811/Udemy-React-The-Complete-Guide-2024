b610780a671bafee1803c10eb2688e1eceb7da71
das972811 <salasdiego54@gmail.com>
2024-11-22 14:49:59 -0800

    Created the Meal Item component

--- 18. Practice Project: Building a Food Order App/01-starting-project/src/components/Meals.jsx
+++ 18. Practice Project: Building a Food Order App/01-starting-project/src/components/Meals.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import MealItem from "./MealItem";
+
 export default function Meals() {
     const [loadedMeals, setLoadedMeals] = useState([]);
 
@@ -20,7 +22,7 @@ export default function Meals() {
 
     return (
         <ul id="meals">
-            {loadedMeals.map(meal => <li key={meal.id}>{meal.name}</li>)}
+            {loadedMeals.map(meal => <MealItem key={meal.id} meal={meal} />)}
         </ul>
     );
 }
